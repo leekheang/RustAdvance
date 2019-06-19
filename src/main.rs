2125f4eeae14f5c7047cf8ac77880ec0c6fa6edf
leekheang <ly.kheang.jr@gmail.com>
2019-06-19 10:59:00 +0700

    create new impl object and debug object

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,25 @@
+#[derive(Debug)]
 struct Object{
     width: u32,
     height: u32,
 }
-
+//Method
 impl Object {
     fn  area (&self) -> u32 {
         self.width * self.height 
     }
-
+    fn show (&self) {
+        println!("{} * {} with area : {} ", self.width, self.height, self.area());
+    }
+}
+// Related Function
+impl Object {
     fn new (width: u32 , height: u32) -> Object {
         Object {
             width,
             height,
-        }
+        }    
     }
-    fn show (&self) {
-        println!("{} * {} with area : {} ", self.width, self.height, self.area());
-    }
-    
 }
 
 fn main() {
@@ -28,4 +30,5 @@ fn main() {
     let obj = Object::new(57, 85);
     o.show();
     obj.show();
+    println!("{:?}", o);
 }
