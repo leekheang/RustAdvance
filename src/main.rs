fe7d43778d7251d8be919898a94418ca04742942
leekheang <ly.kheang.jr@gmail.com>
2019-06-19 11:27:04 +0700

    use fmt for OBJ

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
+use::std::fmt;
 #[derive(Debug)]
 struct Object{
     width: u32,
     height: u32,
 }
 //Method
+
 impl Object {
     fn  area (&self) -> u32 {
         self.width * self.height 
@@ -12,13 +14,21 @@ impl Object {
         println!("{} * {} with area : {} ", self.width, self.height, self.area());
     }
 }
+
 // Related Function
+
 impl Object {
     fn new (width: u32 , height: u32) -> Object {
         Object {
             width,
             height,
         }    
+    }
+}
+
+impl fmt::Display for Object {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result{
+        write!(f,"({} {})", self.width , self.height)
     }
 }
 
@@ -30,6 +40,6 @@ fn main() {
     let obj = Object::new(57, 85);
     o.show();
     obj.show();
-    println!("{:#?}", o);
-    println!("{:#?}", obj);
+    println!("{}", o);
+    println!("{}", obj);
 }
